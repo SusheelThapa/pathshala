95e2bcc7b59cee7ac75e0bef0cb2bcd8e22599d8
SusheelThapa <susheelthapa90@gmail.com>
2024-06-19 08:31:13 +0545

    feat: create channel post dashboard section

---
+++ frontend/src/components/ChannelPosts.tsx
@@ -0,0 +1,135 @@
+import { useState } from "react";
+
+import { ChannelPost } from "../types/types";
+
+interface Props {
+  posts: ChannelPost[];
+  // handleAddNewPost: (newPost: ChannelPost) => void;
+}
+
+const ChannelPosts = ({ posts }: Props) => {
+  const postsPerPage = 3;
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+  const displayPages = 3;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const getPostAgeString = (providedDate: Date): string => {
+    const currentDate = new Date();
+
+    const millisecondsPerSecond = 1000;
+    const millisecondsPerMinute = 60 * millisecondsPerSecond;
+    const millisecondsPerHour = 60 * millisecondsPerMinute;
+    const millisecondsPerDay = 24 * millisecondsPerHour;
+    const millisecondsPerWeek = 7 * millisecondsPerDay;
+    const millisecondsPerMonth = 30.44 * millisecondsPerDay; // Approximate value for a month
+    const millisecondsPerYear = 365.25 * millisecondsPerDay; // Approximate value for a year
+
+    const timeDifference = currentDate.getTime() - providedDate.getTime();
+    const years = Math.floor(timeDifference / millisecondsPerYear);
+    const months = Math.floor(timeDifference / millisecondsPerMonth);
+    const weeks = Math.floor(timeDifference / millisecondsPerWeek);
+    const days = Math.floor(timeDifference / millisecondsPerDay);
+    const hours = Math.floor(timeDifference / millisecondsPerHour);
+    const minutes = Math.floor(timeDifference / millisecondsPerMinute);
+    const seconds = Math.floor(timeDifference / millisecondsPerSecond);
+
+    return years > 0
+      ? `${years} year${years > 1 ? "s" : ""} ago`
+      : months > 0
+      ? `${months} month${months > 1 ? "s" : ""} ago`
+      : weeks > 0
+      ? `${weeks} week${weeks > 1 ? "s" : ""} ago`
+      : days > 0
+      ? `${days} day${days > 1 ? "s" : ""} ago`
+      : hours > 0
+      ? `${hours} hour${hours > 1 ? "s" : ""} ago`
+      : minutes > 0
+      ? `${minutes} minute${minutes > 1 ? "s" : ""} ago`
+      : `${seconds} second${seconds > 1 ? "s" : ""} ago`;
+  };
+
+  const renderPageNumbers = () => {
+    const pages = [];
+    const startPage = Math.max(1, currentPage - Math.floor(displayPages / 2));
+
+    for (
+      let i = startPage;
+      i <= Math.min(totalPages, startPage + displayPages - 1);
+      i++
+    ) {
+      pages.push(
+        <button
+          key={i}
+          onClick={() => paginate(i)}
+          className={`mx-2 p-2  flex justify-center items-center rounded-md font-bold   ${
+            currentPage === i
+              ? "bg-[#f96a46] text-white text-base w-10 h-10"
+              : "bg-gray-200 text-gray-500 text-sm w-8 h-8 hover:bg-[#f96a46] hover:bg-opacity-70 hover:text-white"
+          }`}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    return pages;
+  };
+
+  return (
+    <div className=" text-sm col-span-3 w-full flex flex-col justify-between items-center">
+      {/* <AddCommunityPost handleAddNewPost={handleAddNewPost} /> */}
+      <div className="flex justify-center flex-col items-center gap-8 m-6 w-full">
+        {currentPosts.map(({ postedBy, message, postedOn }) => {
+          return (
+            <div
+              key={postedBy}
+              className="rounded-2xl bg-white px-12 py-10 w-4/5 flex flex-col gap-6"
+            >
+              <div className="flex justify-between items-center">
+                <div className="flex gap-5  items-center">
+                  <div className="text-lg text-gray-600 flex flex-col">
+                    <div>{postedBy}</div>
+                    <div className="text-sm text-stone-400">
+                      {getPostAgeString(postedOn)}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="text-base text-black">{message}</div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="flex justify-center items-center  mt-4 text-lg">
+        {currentPage > 1 && (
+          <button
+            onClick={() => paginate(currentPage - 1)}
+            className="mx-2 p-2 rounded-md  text-gray-500 font-bold hover:text-[#f96a46]"
+          >
+            &lt; Previous
+          </button>
+        )}
+        {renderPageNumbers()}
+        {currentPage < totalPages && (
+          <button
+            onClick={() => paginate(currentPage + 1)}
+            className="mx-2 p-2 rounded-md text-gray-500 font-bold hover:text-[#f96a46]"
+          >
+            Next &gt;
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ChannelPosts;
--- frontend/src/pages/DashBoard.tsx
+++ frontend/src/pages/DashBoard.tsx
@@ -4,7 +4,7 @@ import useAuth from "../hooks/useAuth";
 import { ChannelPost } from "../types/types";
 import { stringToDate } from "../utils/stringToDate";
 import SideBar from "../components/SideBar";
-
+import ChannelPosts from "../components/ChannelPosts";
 import { TbBeta, TbDelta } from "react-icons/tb";
 import {
   SiYoutubegaming,
@@ -47,24 +47,75 @@ const Dashboard: React.FC = () => {
   const { isAuthenticated, loading } = useAuth();
   const [selectedChannel, setSelectedChannel] =
     useState<string>("Daily Notice");
-  const [channelPost, setChannelPost] = useState<ChannelPost>({
-    postedBy: "JohnDoe",
-    postedTo: "Daily Notice",
-    message:
-      "Hello everyone, this is a sample post to the Daily Notice channel.",
-    postedOn: stringToDate("2024-06-19T12:00:00Z"),
-  });
+  const [channelPost, setChannelPost] = useState<ChannelPost[]>([
+    {
+      postedBy: "JohnDoe",
+      postedTo: "Daily Notice",
+      message:
+        "Hello everyone, this is a sample post to the Daily Notice channel.",
+      postedOn: stringToDate("2024-06-19T12:00:00Z"),
+    },
+  ]);
 
   useEffect(() => {
     // Retrieve the message from backend
     // if response is success and check for loading and isAuthenticated
-    setChannelPost({
-      postedBy: "JaneSmith",
-      postedTo: "Alpha Group",
-      message:
-        "Welcome to the Alpha Group! Let's get started with our project.",
-      postedOn: stringToDate("2024-06-19T12:00:00Z"),
-    });
+    setChannelPost([
+      {
+        postedBy: "JaneSmith",
+        postedTo: "Alpha Group",
+        message:
+          "Welcome to the Alpha Group! Let's get started with our project.",
+        postedOn: stringToDate("2024-06-19T12:00:00Z"),
+      },
+      {
+        postedBy: "CarolWhite",
+        postedTo: "Kontribution",
+        message:
+          "Please contribute your ideas for the upcoming event in the Kontribution channel.",
+        postedOn: stringToDate("2024-06-19T12:00:00Z"),
+      },
+      {
+        postedBy: "DaveBlack",
+        postedTo: "Hostel",
+        message:
+          "Hostel residents are invited to a community dinner this weekend.",
+        postedOn: stringToDate("2024-06-19T12:00:00Z"),
+      },
+      {
+        postedBy: "BobBrown",
+        postedTo: "Gaming",
+        message: "Join us for a gaming session tonight at 8 PM.",
+        postedOn: stringToDate("2024-06-19T12:00:00Z"),
+      },
+      {
+        postedBy: "JaneSmith",
+        postedTo: "Alpha Group",
+        message:
+          "Welcome to the Alpha Group! Let's get started with our project.",
+        postedOn: stringToDate("2024-06-19T12:00:00Z"),
+      },
+      {
+        postedBy: "CarolWhite",
+        postedTo: "Kontribution",
+        message:
+          "Please contribute your ideas for the upcoming event in the Kontribution channel.",
+        postedOn: stringToDate("2024-06-19T12:00:00Z"),
+      },
+      {
+        postedBy: "DaveBlack",
+        postedTo: "Hostel",
+        message:
+          "Hostel residents are invited to a community dinner this weekend.",
+        postedOn: stringToDate("2024-06-19T12:00:00Z"),
+      },
+      {
+        postedBy: "BobBrown",
+        postedTo: "Gaming",
+        message: "Join us for a gaming session tonight at 8 PM.",
+        postedOn: stringToDate("2024-06-19T12:00:00Z"),
+      },
+    ]);
   }, [selectedChannel]);
 
   if (loading) {
@@ -79,12 +130,13 @@ const Dashboard: React.FC = () => {
     setSelectedChannel(newSelectedChannel);
   };
   return (
-    <div>
+    <div className="mx-auto my-10 p-4 w-10/12 h-[85vh] grid grid-cols-4 bg-stone-100 rounded-3xl justify-center r align-middle">
       <SideBar
         selectedChannel={selectedChannel}
         onClickChannelListOption={handleChannelSelection}
         sidebarList={sideBarChannelList}
       />
+      <ChannelPosts posts={channelPost} />
     </div>
   );
 };
